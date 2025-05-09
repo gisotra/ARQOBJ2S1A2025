df366a8786c50838e06b59cea115cc0a557a40d6
gisotra <gisotra.henrique@gmail.com>
2025-05-09 08:09:27 -0300

    Aula 5

---
+++ src/aulas/aula5_redes/jogoDaVelha/ClienteJogoDaVelha.java
@@ -0,0 +1,11 @@
+package aulas.aula5_redes.jogoDaVelha;
+
+
+public class ClienteJogoDaVelha {
+    public static void main(String[] args) throws Exception {
+        
+        JogoDaVelha jogo = new JogoDaVelha();
+        
+    }
+}
+
